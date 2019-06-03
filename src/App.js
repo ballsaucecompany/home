1696deb598906102eb710c654b3ffbc7db77016c
r3weber <rweber@osmre.gov>
2019-06-03 16:19:18 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import background from "./backgroundimage.JPG";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+    
       <div className="App">
+        
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        {/* <img src={background} className='App'/> */}
+            <h1>BALL SAUCE COMPANY</h1>
+              
+            <p className="App-intro">
+              'have a bottle of ball'
+            </p>
+              
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        
+      </div> 
+      
+      
+        
+        
     );
   }
 }
